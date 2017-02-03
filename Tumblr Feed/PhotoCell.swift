19b5cbd7aaa61ce110820c2b644069995ed2e4a7
vczhou <victoria.ch.zhou@gmail.com>
2017-02-02 19:07:04 -0600

    Basic photos tumblr feed

---
+++ Tumblr Feed/PhotoCell.swift
@@ -0,0 +1,25 @@
+//
+//  PhotoCell.swift
+//  Tumblr Feed
+//
+//  Created by Victoria Zhou on 2/2/17.
+//  Copyright © 2017 Victoria Zhou. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UITableViewCell {
+
+    @IBOutlet weak var photoView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
